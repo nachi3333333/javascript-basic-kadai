a1b936380742d21a7ec3e52a26c291c0b09ee2da
nachi3333333 <abenachisa315@gmail.com>
2025-02-18 12:28:45 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const day = new Date();
-console.log(day.getFullYear());
-console.log(day.getMonth()+1);
-console.log(day.getDate());
-console.log(day.getFullYear() + "年" + day.getMonth()+1 + "月" + day.getDate() + "日" );
-
