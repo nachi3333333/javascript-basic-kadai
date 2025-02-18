941ea22611c065f98406e01f23a0f1caf31981c0
nachi3333333 <abenachisa315@gmail.com>
2025-02-18 12:29:07 +0900

    3回目

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const day = new Date();
+console.log(day.getFullYear());
+console.log(day.getMonth()+1);
+console.log(day.getDate());
+console.log(day.getFullYear() + "年" + (day.getMonth()+1) + "月" + day.getDate() + "日" );
+
